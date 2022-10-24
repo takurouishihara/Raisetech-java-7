6bcff5ebeb8081004749bd32f190530f1bd7cb24
takurouishihara <k19008@std.nagaokauniv.ac.jp>
2022-10-24 22:45:51 +0900

    feat:：deleteを追加中にエラー　

---
+++ src/main/java/RaiseTech/ControllerGet/CreateForm.java
@@ -0,0 +1,14 @@
+package RaiseTech.ControllerGet;
+
+public class CreateForm {
+    private String name;
+    public String getName() {
+        return name;
+    }
+
+    //おそらくここに何かが足りないと思います。
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/test/java/RaiseTech/ControllerGet/ControllerGetApplicationTests.java
@@ -0,0 +1,13 @@
+package RaiseTech.ControllerGet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControllerGetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
